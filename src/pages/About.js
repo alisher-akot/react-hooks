f07204a398067c316ccbfbeb8f823ec3d12be1d9
Alisher <alisherilyosov2003@gmail.com>
2021-10-11 21:01:00 +0300

    commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => (
+  <div className="jumbotron">
+    <div className="container">
+      <h1 className="display-4">Лучшее React приложение</h1>
+      <p className="lead">
+        Разработал: <strong>Алишер Илёсов</strong>
+      </p>
+    </div>
+  </div>
+)
